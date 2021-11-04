021b4bfb01cf8344621d38532fe37924a56397d0
Jean-philippe Martel <jemartel@c2r5p4.42quebec.com>
2021-11-04 17:10:53 -0400

    made into smaller function

--- path_handle.c
+++ path_handle.c
@@ -3,43 +3,15 @@
 #include <sys/stat.h>
 
 
-int path_trigger(char *path,char **env )
+
+int exec_the_bin(char **paths,char *program, char **env)
 {
-int pid;
-int status;
-
-pid = fork();
-	if(pid < 0)
-		perror("FORK  ERROR");
-	if(pid > 0)
-	{
-		printf(" execve failed ;( %d",execve(path,env,env));
-		
-
-	}
-	waitpid(pid, &status, WEXITED);
-
-
-return(0);
-}
-int path_resolver(char *path_bin,char *program , char **env) 
-{
-	char **paths;
-	char *temp;
 	int inc;
-	int pid;
-	int status;
+	char *temp;
 
 	inc = -1;
-	paths = ft_split(path_bin,':');
-	pid = fork();
-	if(pid < 0)
-		return(-1);
-	if(pid == 0)
-	{
 
-	
-while(paths[++inc])
+	while(paths[++inc])
 	{
 	temp = ft_str3join(paths[inc],"/",program);
 			if(execve(temp,NULL,env) == -1) 
@@ -52,9 +24,30 @@ while(paths[++inc])
 	}
 	printf("command not found %s\n", program);
 	exit(0);
+
+
+return(0);
+}
+
+int path_resolver(char *path_bin,char *program , char **env) 
+{
+	char **paths;
+	int inc;
+	int pid;
+	int status;
+
+	inc = -1;
+	paths = ft_split(path_bin,':');
+	pid = fork();
+	if(pid < 0)
+		return(-1);
+	if(pid == 0)
+	{
+		exec_the_bin(paths,program,env);
 	}
 	if(pid > 0)
 		waitpid(pid,&status,1);
 freelist(paths);
 return(1);
 }
+
