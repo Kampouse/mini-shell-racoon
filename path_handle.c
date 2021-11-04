b181e0b6ed608d31f160be52c621ff25ff4524e3
Jean-philippe Martel <jemartel@c2r5p6.42quebec.com>
2021-11-03 22:10:40 -0400

    no more Zombie FORKS

--- path_handle.c
+++ path_handle.c
@@ -27,22 +27,34 @@ int path_resolver(char *path_bin,char *program , char **env)
 	char **paths;
 	char *temp;
 	int inc;
+	int pid;
+	int status;
 
 	inc = -1;
 	paths = ft_split(path_bin,':');
+	pid = fork();
+	if(pid < 0)
+		return(-1);
+	if(pid == 0)
+	{
+
+	
 while(paths[++inc])
 	{
 	temp = ft_str3join(paths[inc],"/",program);
 			if(execve(temp,NULL,env) == -1) 
 			{
 						free(temp);
-						printf("failed");
 						temp = NULL;			
 			}
 		if(temp)
 			free(temp);
 	}
 	printf("command not found %s\n", program);
+	exit(0);
+	}
+	if(pid > 0)
+		waitpid(pid,&status,1);
 freelist(paths);
 return(1);
 }
