23c44642c63e2e438b2f4048d49ca1ca2b36c7d8
Jean-philippe Martel <jemartel@c1r7p2.42quebec.com>
2021-11-06 17:48:41 -0400

    absolute path and more mudulated code

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include "libft/libft.h"
 #include "minishell.h"
 #include "readline/history.h"
-
+#define GREEN "\002\e[0;32m\002"
+#define RESET   "\001\e[0m\002"
 extern char ** environ;
 char **line_parser(char *line)
 {
@@ -24,6 +25,20 @@ if(ft_strnstr(line,token,len))
 
 return(0);
 }
+int exit_please(char **tokens,char *trimed)
+{
+	if(trimed && tokens)
+	{
+	if(find_token(tokens[0],"exit"))
+	{
+				printf("exit\n");
+				free(trimed);
+				freelist(tokens);
+				return(1);
+	}
+	}
+return(0);
+}
  char	freelist(char **list )
 {
 	int index;
@@ -36,30 +51,25 @@ return(0);
 	free(list);
 	return (0);
 }
-int main (void)
+
+
+int read_wrapper(void)
 {
 
 char *line;
 char *trimed;
 char **tokens;
+
 while(1)
 	{
-		line = readline("minishell:>");
+		line = readline(GREEN"minishell:>"RESET);
 		trimed = ft_strtrim(line," ");
 	    free(line);	
 		if(trimed && ft_strlen(trimed) > 0)
 		{
-				
 				tokens = line_parser(trimed);	
-				if(find_token(trimed,"exit"))
-			{
-			
-				printf("exit\n");
-				free(trimed);
-				freelist(tokens);
-				
-				return(0);
-			}
+				if(exit_please(tokens,trimed) == 1)
+					return (1);
 			path_resolver(findpath(environ),tokens,environ);
 		free(trimed);
 		freelist(tokens);
@@ -67,9 +77,13 @@ while(1)
 		else if(trimed)
 			free(trimed);
 }
+return (0);
+}
 
 
-	
 
-
+int main (void)
+{
+		if(read_wrapper() == 1)
+			printf("delete squense");
 }
--- path_handle.c
+++ path_handle.c
@@ -4,34 +4,32 @@
 #include <sys/stat.h>
 
 
-
+/* last stage of exection of the command if not built in */
 int exec_the_bin(char **paths,char **program, char **env)
 {
 	int inc;
 	char *temp;
 
 	inc = -1;
-
+	execve(program[0],program,env);
 	while(paths[++inc])
 	{
 	temp = ft_str3join(paths[inc],"/",program[0]);
 			if(execve(temp,program,env) == -1) 
 				
 			{
-						free(temp);
-						temp = NULL;			
+				free(temp);
+				temp = NULL;
 			}
 		if(temp)
 			free(temp);
 	}
 	printf("command not found %s\n", program[0]);
 	exit(0);
-
-
 return(0);
 }
-
-int path_resolver(char *path_bin,char **program_args , char **env) 
+/* fork the process  before exectuion  and wait for the child */
+int path_resolver(char *path_bin,char **program_args ,char **env) 
 {
 	char **paths;
 	int inc;
@@ -44,12 +42,9 @@ int path_resolver(char *path_bin,char **program_args , char **env)
 	if(pid < 0)
 		return(-1);
 	if(pid == 0)
-	{
-		exec_the_bin(paths,program_args,env);
-		printf("\n");
-	}
-		waitpid(pid,&status,0);
-freelist(paths);
+		exec_the_bin(paths, program_args, env);
+	waitpid(pid,&status,0);
+	freelist(paths);
 return(1);
 }
 
