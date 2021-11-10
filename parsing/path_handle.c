e0cb1cdd2928a08eafa465f5a410a6c7276599f8
Olivier Labrecque-lacasse <olabrecq@c1r1p1.42quebec.com>
2021-11-10 14:54:15 -0500

    tes fichier add

---
+++ parsing/path_handle.c
@@ -0,0 +1,50 @@
+#include "../libft/libft.h"
+#include "../minishell.h"
+#include <stdio.h>
+#include <sys/stat.h>
+
+
+/* last stage of exection of the command if not built in */
+int exec_the_bin(char **paths,char **program, char **envp)
+{
+	int inc;
+	char *temp;
+
+	inc = -1;
+	execve(program[0],program,envp);
+	while(paths[++inc])
+	{
+		temp = ft_str3join(paths[inc],"/",program[0]);
+		if(execve(temp,program,envp) == -1) 
+		{
+			free(temp);
+			temp = NULL;
+		}
+		if(temp)
+			free(temp);
+	}
+	printf("command not found %s\n", program[0]);
+	exit(0);
+	return(0);
+}
+
+/* fork the process  before exectuion  and wait for the child */
+int path_resolver(char *path_bin, char **program_args, char **envp) 
+{
+	char **paths;
+	int inc;
+	int pid;
+	int status;
+
+	inc = -1;
+	paths = ft_split(path_bin,':');
+	pid = fork();
+	if(pid < 0)
+		return(-1);
+	if(pid == 0)
+		exec_the_bin(paths, program_args, envp);
+	waitpid(pid,&status,0);
+	freelist(paths);
+	return(1);
+}
+
---
+++ parsing/path_handler_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path_handler_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 10:50:06 by jemartel          #+#    #+#             */
+/*   Updated: 2021/11/10 12:54:19 by olabrecq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+
+char	*ft_strend(char *base, char *to_find)
+{
+	char	*basecp;
+	char	*looking;
+	char	*ptr;
+
+	basecp = base;
+	looking = to_find;
+	if (*to_find == 0)
+		return (base);
+	while (*basecp != '\0')
+	{
+		if (*to_find == '\0')
+			return (basecp);
+		ptr = basecp;
+		while (*looking == *basecp && *looking != '\0')
+		{
+			looking++;
+			basecp++;
+		}
+		if (*looking == '\0')
+			return (ptr);
+		basecp++;
+	}
+	return (0);
+}
+
+char	*ft_str3join(const char *first, const char *second, const char *third)
+{
+	char	*output;
+	size_t	len;
+	size_t	len2;
+	size_t	len3;
+
+	len = ft_strlen(first);
+	len2 = ft_strlen(second);
+	len2 = ft_strlen(second);
+	len3 = ft_strlen(third);
+	output = malloc(sizeof(char) * len + len2 + len3 + 1);
+	if (output)
+	{
+		ft_memcpy(output, first, len);
+		ft_memcpy(output + len, second, len2);
+		ft_memcpy(output + len + len2, third, len3 + 1);
+	}
+	return (output);
+}
+
+/* search the path  and  return the path for the excutables */
+char	*findpath(char **environ)
+{
+	int		i;
+	char	*path;
+
+	path = NULL;
+	i = 0;
+	while (environ[i])
+	{
+		if (path)
+			break ;
+		path = ft_strend(environ[i], "PATH=/");
+		i++;
+	}
+	path += ft_strlen("PATH=");
+	return (path);
+}
