95f878fcab5516e2c7ffad727a9ab5754587b967
Olivier Labrecque-lacasse <olabrecq@c2r4p4.42quebec.com>
2021-11-18 19:30:25 -0500

    bit cleaner

--- executing/variable.c
+++ executing/variable.c
@@ -6,44 +6,11 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 17:55:42 by olabrecq          #+#    #+#             */
-/*   Updated: 2021/11/18 19:27:50 by olabrecq         ###   ########.fr       */
+/*   Updated: 2021/11/18 19:30:11 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-// void create_var(char **envp)
-// {
-//     int i;
-//     int j;
-
-//     i = 0;
-//     while (envp[i])
-//     {
-        
-//         i++;
-//     }
-// }
-
-// char	*find_var(char *var)
-// {
-// 	int		i;
-// 	char	*path;
-// 	char 	**tab_search  = g_state.env;
-
-// 	path = var;
-// 	i = 0;
-// 	while (g_state.env[i])
-// 	{
-// 		if (path)
-// 			break ;
-// 		path = ft_strend(g_state.env[i], var);
-// 		i++;
-// 	}
-// 	path += ft_strlen(var);
-// 	return (path);
-// }
-
 
 void print_var(char *var)
 {
--- main.c
+++ main.c
@@ -46,7 +46,7 @@ int main(int argc, char **argv, char **envp)
 	(void)argv;
 	
 	create_env(envp);
-	print_var("$PATH");
+	print_var("$USER");
 	// create_export(envp);
 	// print_exprt(ft_tab_len(envp));
 	// print_env(ft_tab_len(envp));
