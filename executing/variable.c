0520905cfea7b9f1a63d82681ab1ef131146d8d3
Olivier Labrecque-lacasse <olabrecq@c2r6p6.42quebec.com>
2021-11-30 08:51:20 -0500

    add before merge

--- executing/variable.c
+++ executing/variable.c
@@ -6,31 +6,30 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 17:55:42 by olabrecq          #+#    #+#             */
-/*   Updated: 2021/11/29 10:08:07 by olabrecq         ###   ########.fr       */
+/*   Updated: 2021/11/29 14:06:50 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-//DO SEGFAULT 
-// char *print_var(char *var)
-// {
-// 	int i = 0;
-// 	char *temp;
-// 	char *ret;
+char *print_var(char *var)
+{
+	int i = 0;
+	char *temp;
+	char *ret;
 	
-// 	temp = ft_strchr(var, '$');
-// 	//printf("%s\n", temp);
-// 	ret = temp;
-// 	while (g_state.env[i])
-// 	{
-// 		if (ret == ft_strnstr(g_state.env[i], temp, ft_strlen(g_state.env[i])))
-// 		{
-// 			ret = ft_strchr(g_state.env[i], '=');
-// 			printf("%s\n", ret);
-// 			return (ret);
-// 		}
-// 		i++;
-// 	}
-// 	return ("Error");
-// }
+	temp = ft_strchr(var, '$');
+	//printf("%s\n", temp);
+	ret = temp;
+	while (g_state.env[i])
+	{
+		if (ret == ft_strnstr(g_state.env[i], temp, ft_strlen(g_state.env[i])))
+		{
+			ret = ft_strchr(g_state.env[i], '=');
+			printf("%s\n", ret);
+			return (ret);
+		}
+		i++;
+	}
+	return ("Error");
+}
--- main.c
+++ main.c
@@ -2,16 +2,15 @@
 
 t_exec g_state = {0};
 
-
 int main(int argc, char **argv, char **envp)
 {
 	(void)argv;
 
-	if (argc > 1)
-	{
-		ft_putstr("Invalid number of arguments\n");
-		exit (-1);
-	}
+	// if (argc > 1)
+	// {
+	// 	ft_putstr("Invalid number of arguments\n");
+	// 	exit (-1);
+	// }
 	init_exec(envp);
 	parsing();
 }
