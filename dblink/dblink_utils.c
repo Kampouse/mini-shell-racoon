7968ad48691414cb00f616e78690ed65a3f76a8f
Kampouse <jpmartel98@gmail.com>
2022-01-16 01:12:21 +0100

    moar noarm

--- dblink/dblink_utils.c
+++ dblink/dblink_utils.c
@@ -6,7 +6,7 @@
 /*   By: jemartel <jemartel@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 10:46:47 by jemartel          #+#    #+#             */
-/*   Updated: 2022/01/15 22:09:19 by jemartel         ###   ########.fr       */
+/*   Updated: 2022/01/16 01:05:40 by jemartel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 t_dlist	*ft_lst_lastnode(t_dlist *currlist)
 {
-	if( currlist && currlist->next)
+	if (currlist && currlist->next)
 	{
-
 		while (currlist->next)
 		{
 			if (!currlist->next)
@@ -43,70 +42,17 @@ t_dlist	*ft_lst_nextnode(t_dlist *currlist)
 	currlist = currlist->next;
 	return (currlist);
 }
-/*
-int	ft_lst_lenght(t_dlist *currlist)
-{
-	int	len;
-
-	len = 0;
-	while (currlist)
-	{
-		currlist = currlist->next;
-		len++;
-	}
-	return (len);
-}
-*/
-
-t_dlist	*ft_lstnewl(void *content)
-{
-	t_dlist	*link;
-
-	link = (t_dlist *)malloc(sizeof(*link));
-	if (!link)
-		return (NULL);
-	link->content = content;
-	link->next = NULL;
-	return (link);
-}
 
 void	free_list(t_dlist *head)
 {
 	t_dlist	*next;
 
-
-	if(head && head->content)
+	if (head && head->content)
 		free(head->content);
 	if (head != NULL)
 	{
 		next = head->next;
 		free(head);
 		free_list(next);
-	}
-}
-void	ft_lstddelone(t_dlist *lst, void (*del)(void*))
-{
-	if (!del || !lst)
-		return ;
-	if (lst)
-	{
-		(*del)(lst->content);
-		free(lst);
-	}
-}
-
-void	ft_lstdclear(t_dlist **lst, void (*del)(void*))
-{
-	t_dlist	*clear;
-
-	if (!lst || !*lst || !del)
-		return ;
-	while (lst && *lst)
-	{
-		clear = (*lst)->next;
-		
-		ft_lstddelone(*lst, del);
-		free(*lst);
-		*lst = clear;
 	}
 }
