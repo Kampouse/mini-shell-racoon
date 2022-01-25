09856d138d0d11350d0890b621055fe13211861d
Olivier Labrecque-lacasse <olabrecq@c3r7p1.42quebec.com>
2022-01-25 09:37:25 -0500

    before merge

--- bultin/cd.c
+++ bultin/cd.c
@@ -6,97 +6,91 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 08:47:19 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/01/12 09:42:00 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/01/24 11:13:13 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 //  This function update the OLDPWD and PWD variable in the env & export list
-int    update_path(char *old, char *new)
+int	update_path(char *old, char *new)
 {
-    char *to_remove;
-    
-    to_remove = ft_strjoin("OLDPWD=", new);
-    g_state.env = remove_of_list(to_remove, g_state.env);
-    g_state.exprt = remove_of_list(to_remove, g_state.exprt);
-    free(to_remove);
-    to_remove = ft_strjoin("OLDPWD=", old);
-    g_state.env = remove_of_list(to_remove, g_state.env);
-    g_state.exprt = remove_of_list(to_remove, g_state.exprt);
-    g_state.env = add_to_list(to_remove, g_state.env, 0);
-    g_state.exprt = add_to_list(to_remove, g_state.exprt, 1);
-    free(to_remove);
-    to_remove =  ft_strjoin("PWD=", old);
-    g_state.env = remove_of_list(to_remove, g_state.env);
-    g_state.exprt = remove_of_list(to_remove, g_state.exprt);
-    free(to_remove);
-    to_remove =  ft_strjoin("PWD=", new);
-    g_state.env = add_to_list(to_remove, g_state.env, 0);
-    g_state.exprt = add_to_list(to_remove, g_state.exprt, 1);
-    free(to_remove);
-    if (g_state.env == NULL || g_state.exprt == NULL)
-        return (1);
-    return (0);
+	char	*to_remove;
+
+	to_remove = ft_strjoin("OLDPWD=", new);
+	g_state.env = remove_of_list(to_remove, g_state.env);
+	g_state.exprt = remove_of_list(to_remove, g_state.exprt);
+	free(to_remove);
+	to_remove = ft_strjoin("OLDPWD=", old);
+	g_state.env = remove_of_list(to_remove, g_state.env);
+	g_state.exprt = remove_of_list(to_remove, g_state.exprt);
+	g_state.env = add_to_list(to_remove, g_state.env, 0);
+	g_state.exprt = add_to_list(to_remove, g_state.exprt, 1);
+	free(to_remove);
+	to_remove = ft_strjoin("PWD=", old);
+	g_state.env = remove_of_list(to_remove, g_state.env);
+	g_state.exprt = remove_of_list(to_remove, g_state.exprt);
+	free(to_remove);
+	to_remove = ft_strjoin("PWD=", new);
+	g_state.env = add_to_list(to_remove, g_state.env, 0);
+	g_state.exprt = add_to_list(to_remove, g_state.exprt, 1);
+	free(to_remove);
+	if (g_state.env == NULL || g_state.exprt == NULL)
+		return (1);
+	return (0);
 }
 
-int go_home(void)
+int	go_home(void)
 {
-    char **line;
-    int i;
-    bool found;
+	char	**line;
+	int		i;
+	bool	found;
 
-    i = -1;
-    found = false;
-    while (g_state.env[++i])
-    {
-        if (!ft_strncmp("HOME=", g_state.env[i], ft_strlen("HOME=")))
-        {
-            found = true;
-            line = ft_split(g_state.env[i], '=');
-            if (chdir(line[1]))
-            {
-                printf("cd: no such file or directory: %s\n", line[1]);
-                return (1);
-            }
-        }
-    }
-    if (!found)
-    {
-        printf("minishell: cd: HOME not set\n");
-        return (1);
-    }
-    freelist(line);
-    return (0);
+	i = -1;
+	found = false;
+	while (g_state.env[++i])
+	{
+		if (!ft_strncmp("HOME=", g_state.env[i], ft_strlen("HOME=")))
+		{
+			found = true;
+			line = ft_split(g_state.env[i], '=');
+			if (chdir(line[1]))
+			{
+				printf("cd: no such file or directory: %s\n", line[1]);
+				return (1);
+			}
+		}
+	}
+	if (!found)
+		error_message("minishell: cd: HOME not set\n");
+	freelist(line);
+	return (0);
 }
 
-int do_cd(char **args)  
+int	do_cd(char **args)
 {
-    char cwd[4096];
-    char cwd2[4096];
-    
-    if (ft_tab_len(args) == 1)
-    {
-        if (go_home())
-            return (1);
-        return (0);        
-    }
-    if (ft_tab_len(args) > 2)
-    {
-        printf("cd: too many arguments\n");
-		return  (1);
-    }
-    else
-    {
-        getcwd(cwd, sizeof(cwd));
-        if (chdir(args[1]))
-        {
-            printf("cd: no such file or directory: %s\n", args[1]);
-            return (1);
-        }
-        getcwd(cwd2, sizeof(cwd2));
-        if (update_path(cwd, cwd2))
-            return(1);
-    }
-    return (0);
+	char	cwd[4096];
+	char	cwd2[4096];
+
+	if (ft_tab_len(args) == 1)
+	{
+		if (go_home())
+			return (1);
+		return (0);
+	}
+	if (ft_tab_len(args) > 2)
+		error_message("cd: too many arguments\n");
+	else
+	{
+		getcwd(cwd, sizeof(cwd));
+		if (chdir(args[1]))
+		{
+			printf("cd: no such file or directory: %s\n", args[1]);
+			return (1);
+		}
+		getcwd(cwd2, sizeof(cwd2));
+		if (update_path(cwd, cwd2))
+			return (1);
+	}
+	return (0);
 }
--- bultin/echo.c
+++ bultin/echo.c
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 22:49:52 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/01/17 21:17:17 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/01/24 11:19:27 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,32 @@
 
 #include "../minishell.h"
 
-int do_echo(t_jobs *jobs)
+int	do_echo(t_jobs *jobs)
 {
-    int n;
-    int i;
+	int	n;
+	int	i;
 
-    n = 0;
-    i = 1;
-    if (ft_tab_len(jobs->eval) > 1)
-    {
-        if (!ft_strncmp(jobs->eval[1], "-n", (size_t)(sizeof(char) * 2)) && \
-        ft_strlen(jobs->eval[1]) == 2)
-        {
-            n++;
-            i++;
-        }
-        while (jobs->eval[i])
-        {
-            if (jobs->eval[i + 1])
-                printf("%s ", jobs->eval[i++]);
-            else
-                printf("%s", jobs->eval[i++]);
-        }
-    }
-    else
-        return (1);
-    if (!n)
-        printf("\n");
-    return (0);
+	n = 0;
+	i = 1;
+	if (ft_tab_len(jobs->eval) > 1)
+	{
+		if (!ft_strncmp(jobs->eval[1], "-n", (size_t)(sizeof(char) * 2)) && \
+		ft_strlen(jobs->eval[1]) == 2)
+		{
+			n++;
+			i++;
+		}
+		while (jobs->eval[i])
+		{
+			if (jobs->eval[i + 1])
+				printf("%s ", jobs->eval[i++]);
+			else
+				printf("%s", jobs->eval[i++]);
+		}
+	}
+	else
+		return (1);
+	if (!n)
+		printf("\n");
+	return (0);
 }
