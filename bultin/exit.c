d4b4861fc871bb910ad873890c0a952cfb6e1d0b
olabrecq <o.labrecque.lacasse@gmail.com>
2022-01-26 11:37:16 -0500

    casiment fini

--- bultin/exit.c
+++ bultin/exit.c
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 11:01:24 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/01/25 18:43:06 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/01/26 11:33:54 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,35 @@ void quit_shell()
 	exit(g_state.output);
 }
 
-int chek_arg(char *arg)
-{
-	
-}
-
-int	do_exit(char **exit_arg)
+int chek_arg(char **arg)
 {
 	int i;
 
 	i = 0;
-	while (exit_arg[i])
+	if (arg[1])
+	while (arg[i])
 	{
-		if (chek_arg(exit_arg[i++]))
+		i++;
+
+	}
+	return (0);
+}
+
+int	do_exit(char **exit_arg)
+{
+	// int i;
+
+	// i = 0;
+	if (ft_tab_len(exit_arg) > 2)
+		return (1);
+	if (ft_tab_len(exit_arg) < 1)
+	{
+		
+		if (chek_arg(exit_arg))
 			return (1);
+		
 	}
 	quit_shell();
 	return (0);
 }
 
-//Note Exit
-// -> si premier argument est valid et deuxiemen non on exit pas
-// affiche message derreur 
-//si exit sans arg exit(g_state_output)
-//si on envoi un nombre < 256 g_State.output aka $? = ce nombre
-
-// comme premier arg 0 a 255 on affiche 
-//256 = 0
-//257 = 1
-//
