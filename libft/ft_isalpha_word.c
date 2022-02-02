a13e84f56c52656524800012a06644bb24690ca3
Jean-philippe Martel <jemartel@c2r5p6.42quebec.com>
2022-02-01 20:56:11 -0500

    fixed nom

--- libft/ft_isalpha_word.c
+++ libft/ft_isalpha_word.c
@@ -6,7 +6,7 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 12:17:06 by olabrecq          #+#    #+#             */
-/*   Updated: 2022/01/27 09:56:35 by olabrecq         ###   ########.fr       */
+/*   Updated: 2022/02/01 20:55:03 by jemartel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ int	ft_isalpha_word(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+		if ((str[i] >= 'A' && str[i] <= 'Z'))
 			i++;
+		else if ((str[i] >= 'a' && str[i] <= 'z' ))
+				i++;
 		else
 			return (1);
 	}
